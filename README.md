3716a8fc71df4d9ca847c5fa681edfcffdae7bd5
Peijie Hu <phu@rent.com>
2014-11-07 17:45:16 -0800

    "refactor the framework"

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 nodetest1
 =========
+TODO
+git pull/clone on another machine, see if it can pick up the public/lib/***
+=========
 
-<<<<<<< HEAD
-node.js, mongoDB
-=======
 node.js, express, custom framework web app that fetch data from API and present data on web page
->>>>>>> f51442cea8948b0418f393ac96c72c3ec2bc048b
--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -33,7 +33,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Whoops, page not found');
     err.status = 404;
     next(err);
 });
