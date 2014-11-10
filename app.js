ef848bcc4910faac197a00609183e16258af334f
Peijie Hu <phu@rent.com>
2014-11-10 12:17:55 -0800

    production and development env

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// var service = require('./service');
-// service.yahooData("Sunny Vale, CA", "json", function(body) {
-//     console.log(body);
-// });
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views/pages'));
@@ -30,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +51,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    console.log('production error handler');
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
